75fb7f6da8124fe671b93ed729d2f4c8b5e86faf
srottajda <tajda.srot1@gmail.com>
2016-12-27 18:16:08 +0100

    Dodala datoteko za ustvarjanje csv.

--- ustvari_csv.py
+++ ustvari_csv.py
@@ -1,28 +1,34 @@
-import orodja
-import requests
 import re
-import time
+import orodja
 
-def zajemi_podatke():
-    for stran in range(1, 1481, 20):
-        osnovni_naslov = 'http://www.androidrank.org/listcategory?category=&start='
-        parametri = '&sort=4&price=all&hl=en'
-        naslov = '{}{}{}'.format(osnovni_naslov, stran, parametri)
-        ime_datoteke = 'aplikacije, {}.html'.format(stran)
-        orodja.shrani(naslov, ime_datoteke)
-
-def pripravi_aplikacije():
-    regex_aplikacije = re.compile(
-        r'<a href="/application/(?P<povezava>.+?)?hl=en">(?P<ime>.+?)</a>',
+regex_aplikacije = re.compile(
+        r'<small>by <a href=.*?</a><br>\n        (?P<prenosi>.+?)\+ installs</small>.*?'
+        r'<tr><th>Title:</th><td>(?P<ime>.+?)</td></tr>.*?'
+        r'<tr><th>Developer:</th><td><a itemprop="author" href=".*?;hl=en">(?P<proizvajalec>.+?)</a></td></tr>.*?'
+        r'<tr><th>Category:</th><td><a href=".*?;hl=en" itemprop="applicationCategory">(?P<kategorija>.+?)</a></td></tr>.*?'
+        r'<tr><th>Price:</th><td>(?P<cena>.+?)</td></tr>.*?'
+        r'<tr><th>Total ratings:</th><td itemprop="ratingCount">(?P<stevilo_ocen>.+?)</td></tr>.*?'
+        r'<tr><th>Average rating:</th><td itemprop="ratingValue">(?P<ocena>.+?)</td></tr>.*?'
+        ,
         flags=re.DOTALL
     )
 
-    for html_datoteka in orodja.datoteke('app/'):
-        for aplikacija in re.finditer(regex_aplikacije, orodja.vsebina_datoteke(html_datoteka)):
-            naslovcek = '{}{}'.format('http://www.androidrank.org/application/', aplikacija.group('povezava'))
-            imencek = '{}.html'.format(aplikacija.group('ime'))
-            orodja.shrani(naslovcek, imencek)
-            time.sleep(0.1)
 
-zajemi_podatke()
-pripravi_aplikacije()
+def pocisti_aplikacijo(aplikacija):
+    podatki = aplikacija.groupdict()
+    podatki['ocena'] = float(podatki['ocena'])
+    podatki['prenosi'] = int(podatki['prenosi'].replace(',',''))
+    podatki['stevilo_ocen'] = int(podatki['stevilo_ocen'].replace(',',''))
+    return podatki
+
+
+def izloci_podatke_aplikacij(imenik):
+    aplikacije = []
+    for html_datoteka in orodja.datoteke(imenik):
+        for aplikacija in re.finditer(regex_aplikacije, orodja.vsebina_datoteke(html_datoteka)):
+            aplikacije.append(pocisti_aplikacijo(aplikacija))
+    return aplikacije
+
+
+aplikacije = izloci_podatke_aplikacij('app/')
+orodja.zapisi_tabelo(aplikacije, ['ime', 'proizvajalec', 'kategorija', 'cena', 'stevilo_ocen', 'ocena', 'prenosi'], 'aplikacije.csv')
